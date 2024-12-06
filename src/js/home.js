6fc26918dc7fffbc9859893d597e5d413da78569
Richa Jaishwal <richajaiswalhome@gmail.com>
2024-12-06 23:27:46 +0530

    updated

---
+++ src/js/home.js
@@ -0,0 +1,8 @@
+// // script.js
+// window.addEventListener("load", function () {
+//     setTimeout(() => {
+//       document.getElementsByClassName("spinner-grow").style.display = "none"; // Hide spinner
+//       document.getElementByClassName("content").classList.remove("d-none"); // Show content
+//     }, 3000);
+//   });
+  
